c1ae337651b0d44e4c980c1be0d25c4eb030e5fb
dustinfife <fife.dustin@gmail.com>
2019-09-09 13:18:10 -0400

    fixed bug in model.comparison

--- R/model.comparison.R
+++ R/model.comparison.R
@@ -31,7 +31,7 @@ model.comparison = function(model1, model2){
 		anova.res = anova(model1, model2)
 		names(anova.res)
 		p = unlist(anova.res["Pr(>F)"])[2]
-		r.squared = c(summary(a)$r.squared, summary(b)$r.squared)
+		r.squared = c(summary(model1)$r.squared, summary(model2)$r.squared)
 		#1-pchisq( abs(anova.res$Deviance[2]), abs(anova.res$Df[2]))
 	} else {
 		p = NA
