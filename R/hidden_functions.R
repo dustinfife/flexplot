309f3e49a6dd0b62ed149835d3c78c2520070c5b
dustinfife <fife.dustin@gmail.com>
2019-10-17 11:04:42 -0400

    added unit tests

--- R/hidden_functions.R
+++ R/hidden_functions.R
@@ -389,11 +389,7 @@ fit.function = function(outcome, predictors, data, suppress_smooth, method, spre
 			}
 		} else if (spread=="sterr"){	
 			summary1 = "stat_summary(fun.y='mean', geom='point', size=3, position=position_dodge(width=.5), color = '#bf0303')"
-<<<<<<< HEAD
 			summary2 = "stat_summary(geom='errorbar', fun.ymin = function(z){mean(z)-1.96*(sd(z)/sqrt(length(z)-1))}, fun.ymax = function(z){mean(z)+1.96*(sd(z)/sqrt(length(z)-1))}, width=.2, size = 1.25, position=position_dodge(width=.2), color = '#bf0303')"
-=======
-			summary2 = "stat_summary(geom='errorbar', fun.min = function(z){mean(z)-1.96*(sd(z)/sqrt(length(z)-1))}, fun.ymax = function(z){mean(z)+1.96*(sd(z)/sqrt(length(z)-1))}, width=.2, size = 1.25, position=position_dodge(width=.2), color = '#bf0303')"
->>>>>>> master
 			if (mean.line){
 				sum.line = 'stat_summary(aes_string(group= axis[2]), geom="line", fun.y="mean", position=position_dodge(width=.2), color = "#bf0303")'
 			} else {
@@ -402,11 +398,7 @@ fit.function = function(outcome, predictors, data, suppress_smooth, method, spre
 
 		} else if (spread == "quartiles"){	
 			summary1 = "stat_summary(fun.y='median', geom='point', size=3, position=position_dodge(width=.4), color = '#bf0303')" 
-<<<<<<< HEAD
 			summary2 = "stat_summary(geom='errorbar', fun.ymin = function(z){quantile(z, .25)},size = 1.25,  fun.ymax = function(z) {quantile(z, .75)}, fun=median, width=.2, position=position_dodge(width=.4), color = '#bf0303')"
-=======
-			summary2 = "stat_summary(geom='errorbar', fun.min = function(z){quantile(z, .25)},size = 1.25,  fun.ymax = function(z) {quantile(z, .75)}, fun.y=median, width=.2, position=position_dodge(width=.4), color = '#bf0303')"
->>>>>>> master
 			if (mean.line){
 				sum.line = 'stat_summary(aes_string(group=axis[2]), geom="line", fun.y="median", position=position_dodge(width=.4), color = "#bf0303")'
 			} else {
