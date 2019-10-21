e7250687d6d52044a3205103db183543860dd288
Dustin Fife <fife.dustin@gmail.com>
2019-10-21 19:02:03 -0400

    tried jamovi updates

--- build/js/flexplota.events.js
+++ build/js/flexplota.events.js
@@ -6,24 +6,40 @@ const events = {
     onChange_preds: function(ui) {
         updateUI(ui, this);
     },
-    
+
     onChange_given: function(ui) {
-        updateUI2(ui, this);
-    },    
+        updateUI(ui, this);
+    },
 
     // data changed event
     onRemoteDataChanged: function(ui, data) {
         updateUI(ui, this);
-        updateUI2(ui, this);
     }
 };
 
 const updateUI = function(ui, context) {
 
     // change depending on numeric/categorical variable
-    let columnName = ui.preds.value([0]);
-    if (columnName) {
+    let columnNames = ui.preds.value();
+    let givenNames = ui.given.value();
 
+    // if givenNames array is empty or null
+    if ( ! givenNames || givenNames.length === 0) {
+          ui.view.model.options.beginEdit();
+          ui.ghost.setPropertyValue('enable', false);
+          ui.bins.setPropertyValue('enable', false);
+          ui.view.model.options.endEdit();
+    }
+    else {
+        ui.view.model.options.beginEdit();
+        ui.ghost.setPropertyValue('enable', true);
+        ui.bins.setPropertyValue('enable', true);
+        ui.view.model.options.endEdit();
+    }
+
+    // if columnNames array is not empty or null
+    if (columnNames && columnNames.length > 0) {
+        let columnName = columnNames[0];
         //request column info - dataType and measureType
         let promise = context.requestData("column", { columnName: columnName, properties: ["dataType", "measureType"] })
         promise.then(rData => {
@@ -39,29 +55,9 @@ const updateUI = function(ui, context) {
                 ui.center.setPropertyValue('enable', rData.dataType === 'text' );
                 ui.plmethod.setPropertyValue('enable', rData.dataType === 'text' );
                 ui.view.model.options.endEdit();
-            } 
+            }
         });
     }
 };
-
-const updateUI2 = function(ui, context) {
-    let columnName = ui.given.value([0]);
-    if (columnName) {
-      
-        //request column info - dataType and measureType
-        let promise = context.requestData("column", { columnName: columnName, properties: ["dataType", "measureType"] })
-        promise.then(rData => {
-            if (rData.columnFound) {
-                console.log(rData.dataType);
-                console.log(rData.measureType);      
-         
-                ui.view.model.options.beginEdit();
-                ui.ghost.setPropertyValue('enable', rData.dataType === 'integer');      
-                ui.bins.setPropertyValue('enable', rData.dataType === 'integer'); 
-                ui.view.model.options.endEdit();
-            }       
-        });
-  }
-};  
 
 module.exports = events;
--- jamovi/js/flexplota.events.js
+++ jamovi/js/flexplota.events.js
@@ -6,10 +6,10 @@ const events = {
     onChange_preds: function(ui) {
         updateUI(ui, this);
     },
-    
+
     onChange_given: function(ui) {
         updateUI(ui, this);
-    },    
+    },
 
     // data changed event
     onRemoteDataChanged: function(ui, data) {
@@ -20,18 +20,26 @@ const events = {
 const updateUI = function(ui, context) {
 
     // change depending on numeric/categorical variable
-    let columnName = ui.preds.value([0]);
-    let givenName = ui.given.value([0]);
-    
-    if (givenName==="") {
-          ui.view.model.options.beginEdit();
-          ui.ghost.setPropertyValue('enable');      
-          ui.bins.setPropertyValue('enable'); 
-          ui.view.model.options.endEdit();
-    }    
-    
-    if (columnName) {
+    let columnNames = ui.preds.value();
+    let givenNames = ui.given.value();
 
+    // if givenNames array is empty or null
+    if ( ! givenNames || givenNames.length === 0) {
+          ui.view.model.options.beginEdit();
+          ui.ghost.setPropertyValue('enable', false);
+          ui.bins.setPropertyValue('enable', false);
+          ui.view.model.options.endEdit();
+    }
+    else {
+        ui.view.model.options.beginEdit();
+        ui.ghost.setPropertyValue('enable', true);
+        ui.bins.setPropertyValue('enable', true);
+        ui.view.model.options.endEdit();
+    }
+
+    // if columnNames array is not empty or null
+    if (columnNames && columnNames.length > 0) {
+        let columnName = columnNames[0];
         //request column info - dataType and measureType
         let promise = context.requestData("column", { columnName: columnName, properties: ["dataType", "measureType"] })
         promise.then(rData => {
@@ -47,7 +55,7 @@ const updateUI = function(ui, context) {
                 ui.center.setPropertyValue('enable', rData.dataType === 'text' );
                 ui.plmethod.setPropertyValue('enable', rData.dataType === 'text' );
                 ui.view.model.options.endEdit();
-            } 
+            }
         });
     }
 };
