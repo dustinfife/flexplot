f8ba5ad5bed13a7d50978d85fbfd3c14bf126d63
Dustin Fife <fife.dustin@gmail.com>
2019-10-26 20:58:52 -0400

    randomForest wouldn't work with compare.fits/compare.fits error with nested models and NAs
    if one variable in a nested model had NAs, the two datasets would have different sample sizes, which would cause the anova function to throw an error.

--- R/model.comparison.R
+++ R/model.comparison.R
@@ -10,75 +10,97 @@
 ##' @export
 model.comparison = function(model1, model2){
 	
-	#### collect terms
-	mod1 = attr(terms(model1), "term.labels")
-	mod2 = attr(terms(model2), "term.labels")	
-	
-	#### find model types
-	class.mod1 = class(model1)
-	class.mod2 = class(model2)	
-	
-	#### check for nested models
-	if (all(length(mod1)>length(mod2) & (mod2 %in% mod1)) & class.mod1[1] == class.mod2[1]){
-		nested = T
-	} else if (all(length(mod2)>length(mod1) & mod1 %in% mod2)) {
-		nested = T
-	} else {
-		nested = F
-	}
-	
-	if (nested & class(model1) == "lm"){
-		anova.res = anova(model1, model2)
-		names(anova.res)
-		p = unlist(anova.res["Pr(>F)"])[2]
-		r.squared = c(summary(model1)$r.squared, summary(model2)$r.squared)
-		#1-pchisq( abs(anova.res$Deviance[2]), abs(anova.res$Df[2]))
-	} else {
-		p = NA
-		r.squared = c(NA, NA)
-	}
-	
-	m1.name = deparse(substitute(model1))
-	m2.name = deparse(substitute(model2))	
-	aic = c(AIC(model1), AIC(model2))
-	bic = c(BIC(model1), BIC(model2))
-	bayes.factor = bf.bic(model1, model2)
-	
-	model.table = data.frame(aic=aic, bic=bic, bayes.factor=c(bayes.factor, 1/bayes.factor), p.value=c(p, NA), r.squared=r.squared)
-	row.names(model.table) = c(m1.name, m2.name)
-	if (is.na(model.table$p.value[1])){
-		model.table = round(model.table[,!is.na(model.table[1,])], digits=3)	
-	} else {
-		model.table = round(model.table[,!is.na(model.table[1,])], digits=3)
-		model.table[is.na(model.table)] = ""
-		model.table$p.value[1] = format.pval(p, digits=3)
-	}
+  #### find model types
+  class.mod1 = class(model1)
+  class.mod2 = class(model2)
+  
+  #### extract the names
+  m1.name = deparse(substitute(model1))
+  m2.name = deparse(substitute(model2))	
+  
+  #### if class is randomForest, tell them to do something else
+  if (length(grep("randomForest", class.mod1))>0 | length(grep("randomForest", class.mod2))>0){
+    message("Dear Friend. My sincere apologies, but I am limited in my abilities to compare randomForest models. 
+Might I interest you in a suite of other functions, including compare.fits, perhaps? In the mean time, I'll give you *something* useful.")
+    differences=standardized_differences(model1, model2)
+    to.return = differences
+  ### if they have two logistics they're comparing
+  } else if (family(model1)$link=="logit" & family(model2)$link=="logit"){
+    mod1.predictions = sensitivity.table(model1)
+    mod2.predictions = sensitivity.table(model2)	
+    predictions = data.frame(rbind(unlist(mod1.predictions), unlist(mod2.predictions))); row.names(predictions) = c(m1.name, m2.name)
+    if (length(mod1)>length(mod2)){
+      difference = unlist(predictions[1,]) - unlist(predictions[2,])
+    } else {
+      difference = predictions[2,] - predictions[1,]
+    }
+    
+    
+    predictions = data.frame(rbind(predictions, difference)); row.names(predictions)[3] = "Difference"
+    to.return = list(statistics=model.table, predictions=predictions, pred.difference = differences)
+    
+  ### for linear models and other things  
+  } else {
 
-	#### compute difference in predicted value (scaled)
-	mean.sigma = sqrt((sigma(model1)^2 + sigma(model2)^2)/2)
-	differences = round((quantile(abs(predict(model1, type="response") - predict(model2, type="response"))))/mean.sigma, digits=3)
-
-	
-	if (family(model1)$link=="logit" & family(model2)$link=="logit"){
-		mod1.predictions = sensitivity.table(model1)
-		mod2.predictions = sensitivity.table(model2)	
-			predictions = data.frame(rbind(unlist(mod1.predictions), unlist(mod2.predictions))); row.names(predictions) = c(m1.name, m2.name)
-		if (length(mod1)>length(mod2)){
-			difference = unlist(predictions[1,]) - unlist(predictions[2,])
-		} else {
-			difference = predictions[2,] - predictions[1,]
-		}
-		
-
-		predictions = data.frame(rbind(predictions, difference)); row.names(predictions)[3] = "Difference"
-	
-		list(statistics=model.table, predictions=predictions, pred.difference = differences)
-	} else {
-		list(statistics=model.table, pred.difference = differences)
-	}
-	
-	
-
+    #### collect terms
+  	mod1 = attr(terms(model1), "term.labels")
+  	mod2 = attr(terms(model2), "term.labels")	
+  	
+  	#### check for nested models
+  	if (all(length(mod1)>length(mod2) & (mod2 %in% mod1)) & class.mod1[1] == class.mod2[1]){
+  		nested = T
+  	} else if (all(length(mod2)>length(mod1) & mod1 %in% mod2)) {
+  		nested = T
+  	} else {
+  		nested = F
+  	}
+  	
+  	if (nested & class(model1) == "lm"){
+  	  #### check for different Ns for each dataset (if missing data was omitted)
+  	  if (nrow(model1$model) != nrow(model2$model)){
+  	    message(paste0("Note: your models were fit to two different datasets. ",
+  	                   "This is *probably* because you have missing data in one, but not the other.",
+  	                   "I'm going to make the dangerous assumption this is the case and do some ninja moves",
+  	                   " in the background (hiya!). If you don't want me to do this, handle the missing data in advance", sep=""))
+  	    
+  	    ### refit the larger n model with the smaller dataset
+  	    if (nrow(model1$model)>nrow(model2$model)){
+  	      model1 = update(model1, data=model2$model)
+  	    } else {
+  	      model2 = update(model2, data=model1$model)
+  	    }
+  	  }
+  	  
+  	  
+  		anova.res = anova(model1, model2)
+  		p = unlist(anova.res["Pr(>F)"])[2]
+  		r.squared = c(summary(model1)$r.squared, summary(model2)$r.squared)
+  		#1-pchisq( abs(anova.res$Deviance[2]), abs(anova.res$Df[2]))
+  	} else {
+  		p = NA
+  		r.squared = c(NA, NA)
+  	}
+  	
+  	aic = c(AIC(model1), AIC(model2))
+  	bic = c(BIC(model1), BIC(model2))
+  	bayes.factor = bf.bic(model1, model2)
+  	
+  	model.table = data.frame(aic=aic, bic=bic, bayes.factor=c(bayes.factor, 1/bayes.factor), p.value=c(p, NA), r.squared=r.squared)
+  	row.names(model.table) = c(m1.name, m2.name)
+  	if (is.na(model.table$p.value[1])){
+  		model.table = round(model.table[,!is.na(model.table[1,])], digits=3)	
+  	} else {
+  		model.table = round(model.table[,!is.na(model.table[1,])], digits=3)
+  		model.table[is.na(model.table)] = ""
+  		model.table$p.value[1] = format.pval(p, digits=3)
+  	}
+  	
+  	#### compute difference in predicted value (scaled)
+  	differences=standardized_differences(model1, model2)
+  	to.return = list(statistics=model.table, pred.difference = differences)
+  	
+  }
+	return(to.return)
 }
 
 ##' Compute sensitivity/specificity/etc. 
