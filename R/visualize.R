7a4d320516c9e1529e2fd3ea6bb9a8f04d0bafbe
Dustin Fife <fife.dustin@gmail.com>
2020-03-27 12:04:48 -0400

    visualize would not work with glmer

--- R/visualize.R
+++ R/visualize.R
@@ -289,6 +289,189 @@ visualize.lmerMod = function(object, plot=c("all", "residuals", "model"), formul
 
 }
 
+
+#' Visualize a fitted glmerMod model 
+#'
+#' Visualize a fitted glmerMod model
+#' @param object a glmer object
+#' @param plot what should be plotted? Residuals? Model plot? All of them?
+#' @param ... Other arguments passed to flexplot
+#' @param formula A flexplot-style formula
+#' @param sample The number of individuals' datapoints to sample as raw data. Defaults to 3
+#' @param plots.as.list Should the plots be returned as a list? Defaults to FALSE. 
+#' @rawNamespace import(dplyr, except = c(filter, lag))
+#' @export
+visualize.glmerMod = function(object, plot=c("all", "residuals", "model"), formula=NULL, 
+                             sample = 3, plots.as.list=FALSE,...){
+  
+  #### figure out what is numeric
+  d = object@frame
+  plot = match.arg(plot, c("all", "residuals", "model"))
+  
+  #### generate residuals plots
+  res.plots = residual.plots(data=d, object)
+  
+  #### now generate a model plot
+  levels = apply(d, 2, FUN=function(x) length(unique(x)))
+  outcome = names(d)[1]
+  
+  #### extract formula
+  form = as.character(formula(object))[3]
+  
+  #### identify random effects
+  term.re = trimws(substr(form, regexpr("\\|", form)[1]+1, regexpr("\\)", form)[1]-1))		
+  
+  #### find remaining terms
+  preds = names(d)[-1]#[which(!(names(d)[-1] %in% term.re))]
+  
+  #### randomly sample the re terms and convert to numeric
+  samp = sample(unique(d[, term.re]), size=sample)
+  k = d[d[,term.re]%in%samp,]; k[,term.re] = as.factor(k[,term.re])
+  
+  ### come up with formula
+  if (is.null(formula)){
+    slots = c(1,3,4)
+    form.slots = rep(NA, times=4)
+    for (i in 1:min(4,length(preds))){
+      if (preds[i]!=term.re){
+        form.slots[slots[i]] = preds[i]
+      }
+    }
+    
+    ### for random effects models, just put school in first slot
+    if (length(preds)>1) {
+      form.slots[2] = term.re
+    } else {
+      form.slots[1] = term.re
+    }
+    symbol.slots = c("~","+", "|", "+")
+    formula = paste0(symbol.slots, form.slots, collapse="")
+    formula = gsub("\\|NA", "", formula);formula = gsub("\\+NA", "", formula);
+    formula = paste0(outcome, formula, collapse="")
+    
+    formula = formula(formula)
+  } 
+  
+  ### figure out where random component is
+  f.char = as.character(formula)[3]
+  criteria = paste0("\\+.*", term.re)
+  
+  ### if random component is in slot 2, modify the formula
+  if (length(grep(criteria, f.char))>0){
+    modify=T
+    
+    ### if there's a | in the formula, put it back
+    crit2 = paste0("\\+.*", term.re,".*\\|")
+    if (length(grep(crit2, f.char))>0){
+      termses = gsub(crit2, "|", f.char)  
+    } else {
+      termses = gsub(criteria, "", f.char)
+    }
+    
+    formula.new = make.formula(outcome, termses)			
+  } else {
+    modify = F
+  }
+  
+  
+  terms = all.vars(formula)[-1]
+  terms.fixed = terms[-which(terms %in% term.re)]
+  
+  #browser()
+  ##### generate fixed effects predictions
+  #### if random is in NOT in the second slot
+  if (!modify){
+    step3 = compare.fits(formula, data=k, model1=object, model2=object, re=T, ...)
+  } else {
+    #### otherwise...
+    prediction = compare.fits(formula, data=k, model1=object, re=T, return.preds=T)	
+    
+    ### to prevent conflicts with base::filter
+    newd = prediction[prediction$model=="random effects",]; names(newd)[names(newd)=="prediction"] = outcome
+    #newd = prediction %>% dplyr::filter(model=="random effects") %>% dplyr::mutate(MathAch = prediction)			
+    #formula_new = MathAch~SES + School | Sex
+    step3 = flexplot(formula, data=k, suppress_smooth=T, ...) 
+    
+    #browser()
+    #if axis 1 is numeric, do lines
+    if (is.numeric(d[,terms[1]])){
+      m = prediction[prediction$model=="fixed effects",]
+      step3 = step3+ 
+        geom_line(data=m, 
+                  aes_string(terms[1], "prediction", color=NA), linetype=1, lwd=2, col="black") +
+        geom_line(data=newd, 
+                  aes_string(terms[1], outcome, group=term.re, color=term.re))
+      
+      
+      #if axis 1 is categorical, plot means as dots
+    } else {
+      
+      #### see if any variables are binned, then bin the same variables
+      ggdata = step3$data
+      binned.var = names(ggdata)[grep("_binned",names(step3$data))] 
+      unbinned.var = binned = gsub("_binned", "", binned.var)
+      
+      if (length(binned)>0){
+        ### use ggplots data to find num bins and levels
+        bin.levels = levels(step3$data[,binned.var])
+        labs = levels(ggdata[,binned.var])			
+        bins = length(labs)	
+        newd[,binned] = bin.me(variable=binned, data=newd, bins=bins, labels=labs, breaks=NULL, check.breaks=F)				
+        prediction[,binned] = bin.me(variable=binned, data= prediction, bins=bins, labels=labs, breaks=NULL, check.breaks=F)				
+      }			
+      
+      #### aggregate the means across variables		
+      means = prediction %>% group_by_at(vars(one_of(c(terms, "model")))) %>% summarize(Value = mean(prediction))
+      fixed.means = means[means$model=="fixed effects",]
+      fixed.means = fixed.means %>% dplyr::group_by_at(vars(one_of(c(terms.fixed)))) %>% 
+        summarize(Value=mean(Value))
+      
+      means = means[means$model=="random effects",]
+      #means = means %>% dplyr::filter(model=="random effects") 
+      names(means)[ncol(means)] = names(fixed.means)[ncol(fixed.means)] = outcome
+      names(fixed.means)[names(fixed.means)==unbinned.var] = binned.var
+      names(means)[names(means)==unbinned.var] = binned.var			
+      #if (term.re %in% names(fixed.means)) 
+        fixed.means[,term.re] = NA
+        fixed.means[,term.re] = factor(fixed.means[,term.re], levels=levels(means[,term.re]))
+      #head(fixed.means)
+      #### plot it
+      step3 = step3 + 
+        ### fixed effects
+        geom_point(data=fixed.means, aes_string(x=terms[1], y=outcome), size=3, color="black", shape=16) +
+        geom_line(data=fixed.means, aes_string(x=terms[1], y=outcome, group=1), lwd=2, color="black", linetype=1) +
+        
+        ### random effects
+        geom_point(data=means, aes_string(x=terms[1], y=outcome), size=.5) +
+        geom_line(data=means, aes_string(x=terms[1], y=outcome, group=term.re), lwd=.5, linetype=2) 			
+      
+    }	
+    
+    #### remove legend if n>10
+    if (sample>10){
+      step3 = step3 + theme(legend.position="none")
+    }	
+    
+  }
+  
+  
+  #### now put them all together
+  if (plot=="residuals"){
+    p = arrange.plot(histo=res.plots$histo, res.dep=res.plots$res.dep, sl=res.plots$sl, step3=NULL,plot=plot, terms=res.plots$terms, numbers=res.plots$numbers)
+    if (plots.as.list){
+      list(histo=res.plots$histo, res.dep=res.plots$res.dep, sl=res.plots$sl)
+    } else {
+      return(p)
+    }
+  } else if (plot=="model"){
+    return(step3)
+  } else {
+    p = arrange.plot(res.plots$histo, res.plots$res.dep, res.plots$sl, step3, plot, res.plots$terms, res.plots$numbers)
+    return(p)
+  }	
+  
+}
+
 arrange.plot  = function(histo, res.dep, sl, step3, plot, terms, numbers){
 
 	#### return the plots
