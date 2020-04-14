b91cf46c92f2d6eada21e564d1cedb9b61ee939a
Dustin Fife <fife.dustin@gmail.com>
2020-04-14 10:09:55 -0400

    added line plots to jamovi

--- R/glmbasic.h.R
+++ R/glmbasic.h.R
@@ -210,7 +210,8 @@ glmbasicBase <- if (requireNamespace('jmvcore')) R6::R6Class(
                 analysisId = analysisId,
                 revision = revision,
                 pause = NULL,
-                completeWhenFilled = FALSE)
+                completeWhenFilled = FALSE,
+                requiresMissings = FALSE)
         }))
 
 #' General Linear Model
