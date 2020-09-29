7a1411d90d052dc9354c509e6d2699f2bfb9cd01
dustinfife <fife.dustin@gmail.com>
2020-09-29 17:10:30 -0400

    testing cforest had a lot of warnings. Suppressing them.

--- tests/testthat/testcompare_fits.R
+++ tests/testthat/testcompare_fits.R
@@ -103,7 +103,7 @@ test_that("compare.fits for other models", {
   ## compare.fits returned error when data had integers
   d = round(data.frame(y=rnorm(100, 50, 20), x=rnorm(100, 50, 20), z=rnorm(100, 50, 20)))
   class(d$y) = "integer"; class(d$x) = "integer"; class(d$z) = "integer"; 
-  mod = cforest(y~., data=d)
+  mod = suppressWarnings(party::cforest(y~., data=d))
   testthat::expect_true(names(compare.fits(y~x+z, d, mod, return.preds=TRUE))[2]=="z")
   
 })
