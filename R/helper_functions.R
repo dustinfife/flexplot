0c3a682b838116a1b52f789c3c94767a7d14617e
dustinfife <fife.dustin@gmail.com>
2025-06-04 17:11:33 -0400

    added youtube_plot

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -16,3 +16,18 @@ identify_method = function(data, outcome, axis, method=NULL) {
    
   return("loess")
 }
+
+#' Create a 1080p plot for YouTube
+#'
+#' @param p the ggplot object
+#' @param name the name of the file (excluding the ".jpg")
+#' @param path the path to the folder
+#'
+#' @export
+youtube_plot = function(p, name="plot", path=""){
+  require(extrafont)
+  loadfonts(quiet=TRUE)
+  p = p + theme(text=element_text(size=18, family="Moon Flower Bold"))
+  file_path = ifelse(path=="", paste0(name, ".jpg"), paste0(path, "/", name, ".jpg"))
+  ggsave(file_path, plot=p, height = 1080/150, width=1920/150, dpi=150, units = "in")
+}
